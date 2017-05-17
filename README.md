ffaa5c87a15d403ce224cb381f0f889a860aa8f9
Mario Zupan <mzupanmz@gmail.com>
2017-05-17 18:35:37 +0200

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# boltdb-storm-example
+A simple example using BoltDB with Storm and Go
+
+Start with:
+
+```
+go run main.go
+```
+
+This will create a db named `test.db` in the current folder.
---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"github.com/asdine/storm"
+	"github.com/asdine/storm/q"
+	"log"
+	"time"
+)
+
+// Config type
+type Config struct {
+	ID       int `storm:"id,increment"`
+	Height   float64
+	Birthday time.Time
+}
+
+// Entry type
+type Entry struct {
+	ID       int `storm:"id,increment"`
+	Date     time.Time
+	Calories int
+	Food     string
+}
+
+// Weight type
+type Weight struct {
+	ID     int `storm:"id,increment"`
+	Date   time.Time
+	Weight float64
+}
+
+func main() {
+	db, err := storm.Open("test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Config
+	err = addConfig(db, 186, time.Now().AddDate(-30, 0, 0))
+	if err != nil {
+		log.Fatal(err)
+	}
+	var configs []Config
+	err = db.All(&configs, storm.Limit(1), storm.Reverse())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(configs)
+
+	// Weight
+	err = addWeight(db, 86, time.Now())
+	if err != nil {
+		log.Fatal(err)
+	}
+	var weights []Weight
+	err = db.All(&weights, storm.Reverse())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(weights)
+
+	// Entries
+	err = addEntry(db, "apple", 100, time.Now())
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = addEntry(db, "bread", 300, time.Now().AddDate(0, 0, -2))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var today []Entry
+	err = db.Range("Date", time.Now().AddDate(0, 0, -1), time.Now().AddDate(0, 0, 1), &today)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Entries from Today:")
+	fmt.Println(today)
+	var twoDaysAgo []Entry
+	query := db.Select(q.Gt("Date", time.Now().AddDate(0, 0, -3)), q.Lt("Date", time.Now().AddDate(0, 0, -1)))
+	err = query.Find(&twoDaysAgo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Entries from Two Days Ago:")
+	fmt.Println(twoDaysAgo)
+}
+
+func addConfig(db *storm.DB, height float64, birthday time.Time) error {
+	config := Config{Height: height, Birthday: birthday}
+	err := db.Save(&config)
+	if err != nil {
+		return fmt.Errorf("could not save config, %v", err)
+	}
+	fmt.Println("config saved")
+	return nil
+}
+
+func addWeight(db *storm.DB, value float64, date time.Time) error {
+	weight := Weight{Weight: value, Date: date}
+	err := db.Save(&weight)
+	if err != nil {
+		return fmt.Errorf("could not save weight, %v", err)
+	}
+	fmt.Println("weight saved")
+	return nil
+}
+
+func addEntry(db *storm.DB, food string, calories int, date time.Time) error {
+	entry := Entry{Food: food, Calories: calories, Date: date}
+	err := db.Save(&entry)
+	if err != nil {
+		return fmt.Errorf("could not save entry, %v", err)
+	}
+	fmt.Println("entry saved")
+	return nil
+}
